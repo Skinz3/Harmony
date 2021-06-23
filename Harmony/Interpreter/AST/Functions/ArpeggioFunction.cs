d86c1034305bc0eaf18e2b3e2a62957bef36038e
Skinz3 <oliviamarius35@gmail.com>
2021-06-23 09:32:35 +0200

    Add ArpeggioFunction.cs & NotesStatement.cs

---
+++ Harmony/Interpreter/AST/Functions/ArpeggioFunction.cs
@@ -0,0 +1,107 @@
+﻿using Harmony.Interpreter.AST.Statements;
+using Harmony.Sheets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony.Interpreter.AST.Functions
+{
+    public enum StrumTypeEnum
+    {
+        unknown,
+        forward,
+        backward,
+        bidirectional,
+    }
+    public class ArpeggioFunction : Function
+    {
+        private StrumTypeEnum Type
+        {
+            get;
+            set;
+        }
+
+        public ArpeggioFunction(IEntity parent, StrumTypeEnum type) : base(parent)
+        {
+            this.Type = type;
+        }
+
+        protected override void Execute(ref float time, List<SheetNote> notes)
+        {
+            if (Type == StrumTypeEnum.backward)
+            {
+                float totalDuration = Parent.GetLeftDuration();
+
+                float noteDuration = totalDuration / notes.Count;
+
+                var inputNotes = notes.ToArray().Reverse().ToArray();
+
+                var firstNote = inputNotes.First();
+
+                for (int i = 0; i < inputNotes.Length; i++)
+                {
+                    inputNotes[i].Start = firstNote.Start + noteDuration * i;
+                    inputNotes[i].End = inputNotes[i].Start + noteDuration;
+
+                }
+            }
+            if (Type == StrumTypeEnum.forward)
+            {
+                float totalDuration = Parent.GetLeftDuration();
+
+                float noteDuration = totalDuration / notes.Count;
+
+                var firstNote = notes.First();
+
+                for (int i = 0; i < notes.Count; i++)
+                {
+                    notes[i].Start = firstNote.Start + noteDuration * i;
+                    notes[i].End = notes[i].Start + noteDuration;
+
+                }
+            }
+            if (Type == StrumTypeEnum.bidirectional)
+            {
+                var inputNotes = notes.ToArray();
+
+                float totalDuration = Parent.GetLeftDuration();
+
+
+                int resultNotesCount = notes.Count + (inputNotes.Length - 2);
+
+                float noteDuration = (totalDuration / resultNotesCount);
+
+                for (int i = 0; i < inputNotes.Length; i++)
+                {
+                    inputNotes[i].Start += noteDuration * i;
+                    inputNotes[i].End = inputNotes[i].Start + noteDuration;
+                }
+
+                var offsetStart = notes.Last().End;
+
+                inputNotes = inputNotes.Reverse().ToArray();
+
+                for (int i = 1; i < inputNotes.Length - 1; i++)
+                {
+                    float start = offsetStart + noteDuration * (i - 1);
+                    float end = start + noteDuration;
+
+                    SheetNote note = new SheetNote(inputNotes[i].Number, start, end, inputNotes[i].Velocity);
+                    notes.Add(note);
+                }
+            }
+        }
+
+        public override float GetDuration()
+        {
+            return 0f;
+        }
+
+        public override void Prepare()
+        {
+
+        }
+    }
+}
---
+++ Harmony/Interpreter/AST/Statements/NotesStatement.cs
@@ -0,0 +1,54 @@
+﻿using Antlr4.Runtime;
+using Harmony.Notes;
+using Harmony.Sheets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony.Interpreter.AST.Statements
+{
+    public class NotesStatement : Statement
+    {
+        private float Duration
+        {
+            get;
+            set;
+        }
+        private float Velocity
+        {
+            get;
+            set;
+        }
+        private List<Note> Notes
+        {
+            get;
+            set;
+        }
+        public NotesStatement(Unit parent, ParserRuleContext ruleContext, List<Note> notes, float duration, float velocity) : base(parent, ruleContext)
+        {
+            this.Duration = GetTimeSeconds(duration);
+            this.Velocity = velocity;
+            this.Notes = notes;
+        }
+
+        public override float GetDuration()
+        {
+            return Duration;
+        }
+
+        protected override List<SheetNote> Execute(ref float time)
+        {
+            List<SheetNote> results = new List<SheetNote>();
+
+            foreach (var note in Notes)
+            {
+                SheetNote sheetNote = new SheetNote(note.Number, time, time + Duration, Velocity);
+                results.Add(sheetNote);
+            }
+
+            return results;
+        }
+    }
+}
